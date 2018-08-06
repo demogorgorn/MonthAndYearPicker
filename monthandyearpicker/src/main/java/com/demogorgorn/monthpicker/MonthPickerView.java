71c3e24633d1d322d1ebfe1ddb984a74ab05fabf
Oleg Martemjanov <demogorgorn@gmail.com>
2018-08-06 21:02:52 +0300

    fix buttons 1

--- monthandyearpicker/src/main/java/com/demogorgorn/monthpicker/MonthPickerView.java
+++ monthandyearpicker/src/main/java/com/demogorgorn/monthpicker/MonthPickerView.java
@@ -221,12 +221,12 @@ class MonthPickerView extends FrameLayout {
             cancel.setTextColor(headerBgColor);
         }
 
-        if (_positiveText == -1) {
+        if (_positiveText != -1) {
             ok.setText(getContext().getResources().getString(R.string.positive_text));
         } else
             ok.setText(_positiveText);
 
-        if (_negativeText == -1) {
+        if (_negativeText != -1) {
             cancel.setText(getContext().getResources().getString(R.string.negative_text));
         } else
             cancel.setText(_negativeText);
